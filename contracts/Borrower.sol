05a4998fdbafa330f4b8e78b7587960f1c4b3319
Adam Skrodzki adamskrodzki@gmail.com <you@example.com>
2019-11-03 19:39:38 +0100

    allow different eth sources during borrow

--- contracts/Borrower.sol
+++ contracts/Borrower.sol
@@ -6,17 +6,22 @@ import "./ILegalCaller.sol";
 contract Borrower{
 
     address public tokenAddress;
+    bool public inBorrow;
     using SafeMath for uint256;
 
     function init(address _tok) external{
         require(tokenAddress == address(0),"can set token address only once");
         tokenAddress = _tok;
     }
-    function getFee(uint amount) public view returns(uint){
+    /*
+        In ffuture this will be fetched from settings contract
+    */
+    function getFee(uint amount) public pure returns(uint){
         return amount/100000;
     }
     function borrow(uint amount) external{
         uint earnings = 0;
+        inBorrow = true;
         require(tokenAddress != address(0),"token needs to be set");
         require(amount>0,"amount must be positive");
         require(amount<=address(tokenAddress).balance,"funds not sufficient to borrow that much");
@@ -25,14 +30,15 @@ contract Borrower{
         ILegalCaller(msg.sender).invoke.value(amount)();
         uint amountOnEnd = address(this).balance;
         require(amountOnStart<amountOnEnd, "not all funds returned");
-        require(amountOnStart<=amountOnEnd+getFee(amount), "fee notPaid");
+        require(amountOnStart<=amountOnEnd+getFee(amount), "fee not paid");
         earnings = amountOnEnd-amountOnStart;
         earnings = earnings.mul(95).div(100);
         TxLendToken(tokenAddress).giveBack.value(amount+earnings)(amount);
+        inBorrow = false;
     }
 
     function () external payable {
-        require(msg.sender == address(tokenAddress),"only token can send funds to borrower");
+        require(msg.sender == address(tokenAddress) || inBorrow,"only token can send funds to borrower");
     }
 
 }
